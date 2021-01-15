5d5d4fefcdabeb48668e6e470fcf646123cb95ef
Kody Samaroo <github email address>
2021-01-14 22:08:07 -0500

    commit tree f5363f3a284022bb679e881c22ef239f4298da73

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,13 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+    @student = Student.new
+  end
+
+  def create
+    student = Student.create(student_params)
+    redirect_to new_student_path
+  end
+
 end
